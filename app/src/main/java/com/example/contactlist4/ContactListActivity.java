cbb7f0acbc728ee4b4090d1159b1a2d19b876b71
omarm <omarumustafa@gmail.com>
2025-02-04 11:45:49 -0500

    I have the contact list toggle button working and all the image buttons working

--- app/src/main/java/com/example/contactlist4/ContactListActivity.java
+++ app/src/main/java/com/example/contactlist4/ContactListActivity.java
@@ -1,6 +1,11 @@
 package com.example.contactlist4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,69 @@ public class ContactListActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        contactlistButton();
+        mapButton();
+        settingsButton();
+        initToggleButton();
+
+        setForEditing(false);
+    }
+
+    private void contactlistButton() {
+        ImageButton imageButtonList = findViewById(R.id.imageButtonList);
+        imageButtonList.setOnClickListener(v -> {
+            Intent intent = new Intent(ContactListActivity.this, ContactListActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void mapButton() {
+        ImageButton imageButtonMap = findViewById(R.id.imageButtonMap);
+        imageButtonMap.setOnClickListener(v -> {
+            Intent intent = new Intent(ContactListActivity.this, ContactMapActivity.class);
+            startActivity(intent);
+        });
+    }
+    private void settingsButton() {
+        ImageButton imageButtonSettings = findViewById(R.id.imageButtonSettings);
+        imageButtonSettings.setOnClickListener(v -> {
+            Intent intent = new Intent(ContactListActivity.this, ContactSettingsActivity.class);
+            startActivity(intent);
+        });
+    }
+    private void initToggleButton() {
+        ToggleButton toggleButtonEdit = findViewById(R.id.toggleButtonEdit);
+        toggleButtonEdit.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            setForEditing(isChecked);
+        });
+
+    }
+
+    private void setForEditing(boolean isChecked) {
+        EditText editTextName = findViewById(R.id.editTextName);
+        EditText editTextStreet = findViewById(R.id.editTextStreet);
+        EditText editTextCity = findViewById(R.id.editTextCity);
+        EditText editTextState = findViewById(R.id.editTextState);
+        EditText editTextZip = findViewById(R.id.editTextZip);
+        EditText editTextHomePhone = findViewById(R.id.editTextHomePhone);
+        EditText editTextCellPhone = findViewById(R.id.editTextCellPhone);
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+        Button buttonChange = findViewById(R.id.buttonChange);
+        Button buttonSave = findViewById(R.id.buttonSave);
+
+        editTextHomePhone.setEnabled(isChecked);
+        editTextCellPhone.setEnabled(isChecked);
+        editTextEmail.setEnabled(isChecked);
+        buttonChange.setEnabled(isChecked);
+        buttonSave.setEnabled(isChecked);
+
+
+        editTextName.setEnabled(isChecked);
+        editTextStreet.setEnabled(isChecked);
+        editTextCity.setEnabled(isChecked);
+        editTextState.setEnabled(isChecked);
+        editTextZip.setEnabled(isChecked);
+
+
     }
 }
--- app/src/main/java/com/example/contactlist4/MainActivity.java
+++ app/src/main/java/com/example/contactlist4/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.contactlist4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        contactlistButton();
+        mapButton();
+        settingsButton();
+
     }
+
+    private void contactlistButton() {
+        ImageButton imageButtonList = findViewById(R.id.imageButtonList);
+        imageButtonList.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ContactListActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void mapButton() {
+        ImageButton imageButtonMap = findViewById(R.id.imageButtonMap);
+        imageButtonMap.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ContactMapActivity.class);
+            startActivity(intent);
+        });
+    }
+    private void settingsButton() {
+        ImageButton imageButtonSettings = findViewById(R.id.imageButtonSettings);
+        imageButtonSettings.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ContactSettingsActivity.class);
+            startActivity(intent);
+        });
+    }
+
 }
+
+
+
